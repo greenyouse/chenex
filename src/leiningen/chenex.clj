dca702a21e9550eb3f2842bce6422564cffb5838
greenyouse <greenyouse@hotmail.com>
2015-04-12 05:30:02 -0500

    Add package command

--- src/leiningen/chenex.clj
+++ src/leiningen/chenex.clj
@@ -3,8 +3,10 @@
             [leiningen.core.main :as lmain]
             [leiningen.core.eval :refer (eval-in-project)]
             [clojure.pprint :refer [pprint]]
-            [clojure.java.io :as io])
-  (:refer-clojure :exclude [compile]))
+            [clojure.java.io :as io]
+            [clojure.java.shell :as sh])
+  (:refer-clojure :exclude [compile])
+  (import java.io.File))
 
 (defn- yellow-text [msg] (str "\033[33m" msg "\033[0m"))
 (defn- red-text [msg] (str "\033[31m" msg "\033[0m"))
@@ -25,6 +27,7 @@
       (if log#
         (lmain/info (red-text "Chenex Error: No builds detected in project.clj"))))))
 
+
 ;;happy to add more templates here
 (defn- build
   "Writes a basic build template, options are: cljx"
@@ -37,6 +40,7 @@ Options are: cljx")))))]
     (do (io/make-parents "builds/chenex-builds.clj")
         (spit "builds/chenex-builds.clj" loc#))))
 
+
 (defn- repl
   "Sets a new target environment for the REPL iteractively. Enter the name
   of a chenex feature environement"
@@ -47,10 +51,36 @@ Options are: cljx")))))]
              (spit "builds/chenex-repl.clj" (str e#)))
          (catch Exception _ (lmain/abort (red-text "Chenex Error: enter the name of a chenex feature\n\n"))))))
 
+
+(defn- write-extension [f filetype]
+  (clojure.string/replace f #"\.cljx$" (str "." filetype)))
+
+(defn- mv-files [cljx out]
+  (map #(sh/sh "mv" % %2) cljx out))
+
+(defn- package
+  "Takes a filetype and sets all cljx files in the source-paths to it so they
+  can be consumed nicely by downstream projects. Use this if you're writing a
+  library with chenex."
+  [filetype project]
+  (if (or (nil? (some #(#{:clj :cljs} %) (map keyword filetype)))
+        (not= 1 (count filetype)))
+    (lmain/abort (red-text "Chenex Error: package requires either clj or cljs"))
+    (let [src (:source-paths project)
+          files (reduce #(into % (file-seq (File. %2))) [] src)
+          cljx? (fn [f]
+                  (re-find #"^(?:(?!#).)*\.cljx$" (str f)))
+          cljx-files (->> files
+                   (filter cljx?)
+                   (map str))
+          new-files (map #(write-extension % (first filetype)) cljx-files)]
+      (doall (mv-files cljx-files new-files)))))
+
+
 (defn chenex
   "Run the chenex compiler"
-  {:help-arglists '([compile build repl])
-   :subtasks [#'compile #'build #'repl]}
+  {:help-arglists '([compile build repl package])
+   :subtasks [#'compile #'build #'repl #'package]}
   ([project]
      (lmain/abort
       (lhelp/help-for "chenex")))
@@ -59,5 +89,6 @@ Options are: cljx")))))]
        "compile" (compile project)
        "build" (build args)
        "repl" (repl args)
+       "package" (package args project)
        (lmain/abort (red-text (str "Chenex Error: Subtask " subtask " not found."))
                     (lhelp/subtask-help-for "chenex")))))
